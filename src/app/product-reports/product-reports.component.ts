59352c162f919a4bb70f19dbb4f6e1c1d871f60a
prabhakarikonicit <prabhakar.koppireddy@ikonicit.com>
2023-03-02 23:53:22 +0530

    changes
    changes

--- src/app/product-reports/product-reports.component.ts
+++ src/app/product-reports/product-reports.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-product-reports',
   templateUrl: './product-reports.component.html',
   styleUrls: ['./product-reports.component.css']
 })
 export class ProductReportsComponent implements OnInit {
+  allFlag = false;
+  brandFlag = false;
+  localeFlag = false;
+  searchForm = this.formBuilder.group({
+    productReportType: [''],
+  });
+  constructor( private readonly formBuilder: FormBuilder,) { }
 
-  constructor() { }
-
+  getFileContent() {
+    window.location.assign(
+      ".assets/reprotfiles/ALL_PRODUCTS_REPORT.PDF"
+    );
+  }
   ngOnInit(): void {
   }
 
+  searchProductReport() {
+   const proReportType = this.searchForm.get('productReportType')?.value;
+   if(proReportType === 'ALL'){
+    this.allFlag = true;
+    this.brandFlag = false;
+    this.localeFlag = false;
+   }else if(proReportType === 'BRAND'){
+    this.brandFlag = true;
+    this.allFlag = false;
+    this.localeFlag = false;
+   }else if(proReportType === 'LOCALE'){
+    this.allFlag = false;
+    this.brandFlag = false;
+    this.localeFlag = true;
+   }else{
+    this.allFlag = false;
+    this.brandFlag = false;
+    this.localeFlag = false;
+   }
+    console.log("productReportType",proReportType);
+  }
 }
